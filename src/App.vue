48970c26988749230bd6f5aa4a8bbbef9396a3c4
madzharov <ih6UTqZ5>
2019-04-29 19:30:09 +0300

    Добавил компоненты: 1. TodoList 2. Todo 3. CreateTodo

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,52 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <todo-list :todos="todos"></todo-list>
+    <create-todo v-on:create-todo="addTodo"/>
   </div>
 </template>
 
 <script>
+  import TodoList from '@/components/TodoList';
+  import CreateTodo from '@/components/CreateTodo';
+
 export default {
-  name: 'App'
+  name: 'App',
+  components: {TodoList,CreateTodo},
+  data(){
+      return {
+          todos: [{
+              id: 1,
+              title: 'Todo A',
+              project: 'Project A',
+              done: false,
+          }, {
+              id: 2,
+              title: 'Todo B',
+              project: 'Project B',
+              done: true,
+          }, {
+              id: 3,
+              title: 'Todo C',
+              project: 'Project C',
+              done: false,
+          }, {
+              id: 4,
+              title: 'Todo D',
+              project: 'Project D',
+              done: false,
+          }],
+      };
+  },
+  methods: {
+      addTodo(obj) {
+          this.todos.push({
+              id: this.todos.length+1,
+              title: obj.title,
+              project: obj.project,
+              done: false,
+          });
+      },
+  },
 }
 </script>
 
---
+++ src/components/TodoList.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        // JavaScript expressions in Vue are enclosed in double curly brackets.
+        <p>Completed Tasks: {{todos.filter(todo => {return todo.done === true}).length}}</p>
+        <p>Pending Tasks: {{todos.filter(todo => {return todo.done === false}).length}}</p>
+
+        <todo  v-for="todo in todos" :todo="todo" :key="todo.id"
+        v-on:delete-todo="deleteTodo"></todo>
+    </div>
+</template>
+
+<script type = "text/javascript" >
+    import Todo from '@/components/Todo';
+
+    export default {
+        name: "TodoList",
+        components: {Todo},
+        props: ['todos'],
+        methods: {
+            deleteTodo(todo) {
+                const todoIndex = this.todos.indexOf(todo);
+                this.todos.splice(todoIndex, 1);
+            },
+        },
+    };
+</script>
